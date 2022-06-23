ab3c4d51bd36f2b3ac5d1d24ec8d5b28aad231da
Seungmin Jeong <seungmin1266@gmail.com>
2022-06-24 01:19:49 +0900

    [Update Map.js] : Testing hover

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,21 +1,27 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import world from '../../Sources/Map/world.json';
 import { VectorMap } from '@south-paw/react-vector-maps';
 
 const Map = () => {
-    const style = { margin: '1rem auto', width: '300px'};
+    const style = { margin: '1rem auto', width: '50%'};
+   
+    
 
     const [hovered, setHovered] = useState('None');
     const [focused, setFocused] = useState('None');
     const [clicked, setClicked] = useState('None');
+    const [color, setColor] = useState('red');
 
     const layerProps = {
         onMouseEnter: ({ target }) => setHovered(target.attributes.name.value),
         onMouseLeave: ({ target }) => setHovered('None'),
         onFocus: ({ target }) => setFocused(target.attributes.name.value),
         onBlur: ({ target }) => setFocused('None'),
-        onClick: ({ target }) => setClicked(target.attributes.name.value),
+        onClick: ({ target }) => setColor("blue"),
     };
+
+    console.log(color);
+    
 
     return(
         <div style={style}>
@@ -23,7 +29,7 @@ const Map = () => {
             <hr />
             <p>Hovered: {hovered && <code>{hovered}</code>}</p>
             <p>Focused: {focused && <code>{focused}</code>}</p>
-            <p>Clicked: {clicked && <code>{clicked}</code>}</p>
+            <p>Clicked: {color && <code>{color}</code>}</p>
         </div>
     )
 }
