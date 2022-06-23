4673a72b716cdbcbf2b5f462e4fa4c9c88d7643d
Seungmin Jeong <seungmin1266@gmail.com>
2022-06-23 21:59:23 +0900

    [Updated Domestic.js] : cumulative count data

--- src/Components/Domestic/Domestic.js
+++ src/Components/Domestic/Domestic.js
@@ -1,18 +1,36 @@
 import React from "react";
 import Graph from "../Graph/Graph";
+import "./Domestic.css";
+import Data from "../../testData";
 
 
 const Domestic = () => {
-    return(
-        <div>
-            <h1>국내 확진 추이</h1>
-            <div id = "divDomesticGraph">
-                <Graph/>
-            </div> {/* End of divDomesticGraph */}
+    var countCnt = 0;
 
-            <div id = "divNumberofDomestic">
-                <p>대한민국 확진 현황</p>
-                <p>최근 확진일</p>
+    const newArrayData = Data.map((item, index) => {
+        countCnt += item.count;
+        console.log(index);
+        return (
+          <li key={index}>
+            {item.name}({item.count})
+          </li>
+        );
+      });
+
+    return(
+        <div id = "divDomesticWrapper">
+            <h1>국내 확진 추이</h1>
+            <div id = "divDomesticCount">
+                <div id = "divDomesticGraph">
+                    <Graph/>
+                </div> {/* End of divDomesticGraph */}
+
+                <div id = "divNumberofDomestic">
+                    <p>대한민국 확진 현황</p><br/>
+                    누적 : { countCnt } <br/>
+                    오늘 : <br/>
+                    { newArrayData }
+                </div>
             </div>
         </div>
     )
