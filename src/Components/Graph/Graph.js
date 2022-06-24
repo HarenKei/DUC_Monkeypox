a982a6cfed9c3684596be353b73bfbb1ce160117
Seungmin Jeong <seungmin1266@gmail.com>
2022-06-24 22:31:58 +0900

    Updated

--- src/Components/Graph/Graph.js
+++ src/Components/Graph/Graph.js
@@ -8,41 +8,6 @@ import {
     Tooltip
 } from "recharts";
 import testData from '../../testData.json';
-<<<<<<< HEAD
-
-
-
-  const Graph = () => {
-      return(
-          <div>
-              <BarChart
-                    width={900}
-                    height={400}
-                    data={ testData }
-                    margin={{
-                        top: 5,
-                        right: 0,
-                        left: 0,
-                        bottom: 25
-                    }}
-                >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-
-                <Bar
-                  type="monotone"
-
-                  dataKey="count"
-                  fill="#8884d8"
-                  barSize={30}
-                />
-                </BarChart>
-          </div>
-      )
-    };
-=======
 import "./Graph.css";
 
 const Graph = () => {
@@ -72,6 +37,5 @@ const Graph = () => {
         </div>
     )
 };
->>>>>>> 44337212791c91f4616cb71b7f45063b5d766ad8
 
 export default Graph;
--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -4,7 +4,6 @@ import { VectorMap } from '@south-paw/react-vector-maps';
 import styled, {css} from 'styled-components';
 import "./Map.css";
 import ExamData from '../../Sources/example.json';
-import "./Map.css";
 
 
 
@@ -17,28 +16,16 @@ const Map = () => {
         );
     });
     
-
-
-    function forItem(items){
-        for(var i = 0; i < items.length; i++){
-            return(css`
-                    &[id="${items[i].id}"]{
-                        fill:red;
-                    }
-            `);
-        }
-    }
     
     let items = Object.values(ExamData);
     var countryLength = items.length;
    
     
    const Map = styled.div`
-        width:920px;
-        margin:30px;
+        margin: 1rem auto;
+        width: 820px;
 
         svg{
-<<<<<<< HEAD
             stroke: #000;
 
             path {
@@ -52,46 +39,12 @@ const Map = () => {
           
                 &:hover {
                   fill: pink;
-=======
-            stroke: #fff;
-            path{
-                fill: grey;
-                cursor: pointer;
-                outline: none;
-                &[id="ru"]{
-                    fill : red;
-                }
-                &[id="us"]{
-                    fill : red;
-                }
-                &:hover {
-                  fill: pink;
-                  &[id="ru"]{
-                    fill: pink;
-                }
-                }
-                path{
-                    &[id="ru"]{
-                        
-                    }
-                    
->>>>>>> 44337212791c91f4616cb71b7f45063b5d766ad8
                 }
             }
         }
    `;
-<<<<<<< HEAD
    const [current, setCurrent] = useState(null);
    const [hovered, setHovered] = useState('None');
-=======
-
-    const style = { margin: '1rem auto', width: '300px'};
-    const [current, setCurrent] = useState(null);
-   
-
-    const [hovered, setHovered] = useState('None');
-    const [color, setColor] = useState('grey');
->>>>>>> 44337212791c91f4616cb71b7f45063b5d766ad8
 
     const layerProps = {
         onMouseEnter: ({ target }) => setHovered(target.attributes.name.value),
@@ -106,14 +59,6 @@ const Map = () => {
     var month = now.getMonth() + 1;
     var day = now.getDate();
     
-<<<<<<< HEAD
-=======
-    var now = new Date();
-    var year = now.getFullYear();
-    var month = now.getMonth() + 1;
-    var day = now.getDate();
-    
->>>>>>> 44337212791c91f4616cb71b7f45063b5d766ad8
     return(
         <div id="map">
             <p>전세계 누적 확진자: 0명</p>
